01b29590a98f4b5701cd302c7733bae9bf3e3337
AndyYou <andyyu0920@gmail.com>
2015-07-26 15:09:55 +0800

    Initial and Finish 1.0 version

---
+++ index.js
@@ -0,0 +1,14 @@
+var Xray = require("x-ray");
+var x = Xray();
+
+x("http://ioh.tw/talks", "article.talk", [{
+  url: "> a@href",
+  name: "> a@title",
+  avatar: "img@src",
+  school: x(".schools", ["a"]),
+  department: ".category a",
+  description: "p@html", 
+}])
+.paginate(".pager > .next")
+.limit(15)
+.write("result.json")
